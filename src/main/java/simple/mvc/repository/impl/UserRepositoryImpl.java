31f4d433a8e8a564c2dc6550af899e56b59344d3
unknown <t33rmin41@gmail.com>
2017-07-02 21:17:32 +0300

    Added unimplemented method

--- src/main/java/simple/mvc/repository/impl/UserRepositoryImpl.java
+++ src/main/java/simple/mvc/repository/impl/UserRepositoryImpl.java
@@ -1,7 +1,14 @@
 package simple.mvc.repository.impl;
 
+import simple.mvc.jpa.User;
 import simple.mvc.repository.UserRepository;
 
 public class UserRepositoryImpl implements UserRepository {
+
+    @Override
+    public User getUserByUsernameAndPassword(String username, String password) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
