a07442b847124c08146fe9f4513b07638c3e0e1e
t3rmin41 <t33rmin41@gmail.com>
2017-08-14 12:29:05 +0300

    Added LogbackValve for Tomcat, access log to console OK, siftappender not started, need to fix

--- src/main/java/simple/mvc/config/AppConfig.java
+++ src/main/java/simple/mvc/config/AppConfig.java
@@ -1,24 +1,33 @@
 package simple.mvc.config;
 
+import javax.servlet.Servlet;
+
+import org.apache.catalina.startup.Tomcat;
 import org.h2.server.web.WebServlet;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import ch.qos.logback.access.tomcat.LogbackValve;
+
 @Configuration
 @ComponentScan(basePackages = {"simple.mvc.controller", "simple.mvc.service.impl", "simple.mvc.repository.impl", "simple.mvc.app.mapper.impl", "simple.mvc.util"})
 @EntityScan(basePackages = {"simple.mvc.jpa"})
 public class AppConfig {
 
-  @Value("${spring.profiles.active}")
-  private String activeProfile;
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
     
-  @Bean(name = "dataSource")
-  public DriverManagerDataSource dataSource() {
+    @Bean(name = "dataSource")
+    public DriverManagerDataSource dataSource() {
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
         driverManagerDataSource.setDriverClassName("org.h2.Driver");
         driverManagerDataSource.setUsername("sa");
@@ -30,13 +39,28 @@ public class AppConfig {
             driverManagerDataSource.setPassword("d3vs3cr3t");
         }
         return driverManagerDataSource;
-  }
+    }
     
-  @Bean
-  public ServletRegistrationBean h2servletRegistration(){
-      ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
-      registrationBean.addUrlMappings("/h2-console/*");
-      return registrationBean;
-  }
+    @Bean
+    public ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+        registrationBean.addUrlMappings("/h2-console/*");
+        return registrationBean;
+    }
 
+    @Bean
+    @ConditionalOnClass({ Servlet.class, Tomcat.class }) 
+    public EmbeddedServletContainerCustomizer containerCustomizer(){
+        return new EmbeddedServletContainerCustomizer() {
+          @Override
+          public void customize(ConfigurableEmbeddedServletContainer factory) {
+            if(factory instanceof TomcatEmbeddedServletContainerFactory){
+              TomcatEmbeddedServletContainerFactory containerFactory = (TomcatEmbeddedServletContainerFactory) factory;
+              LogbackValve logbackValve = new LogbackValve();
+              logbackValve.setFilename("src/main/resources/logback-access.xml");
+              containerFactory.addContextValves(logbackValve);
+            }
+          }
+        };
+    }
 }
